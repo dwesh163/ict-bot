499b7eb86e9425f2856736b17561207ee110f222
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-01-14 18:56:55 +0100

    [feature] Implement bot functionality with start and modules commands

---
+++ index.js
@@ -0,0 +1,30 @@
+const { Telegraf, Markup } = require('telegraf');
+const dotenv = require('dotenv');
+const { getModules, getJobs, getModuleByJob } = require('./modules/api');
+const { getModulesText, getText } = require('./modules/message');
+const { getLang } = require('./modules/languages');
+
+dotenv.config();
+
+console.log('Starting bot');
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.start((ctx) => ctx.reply(getText('start', getLang(ctx.from.id))));
+bot.help((ctx) => ctx.reply('Send me a sticker or use /modules to start.'));
+
+bot.command('modules', async (ctx) => {
+	const lang = getLang(ctx.from.id);
+
+	try {
+		const jobs = await getJobs(lang);
+		await ctx.reply(getText('modules', lang), {
+			parse_mode: 'Markdown',
+			reply_markup: {
+				inline_keyboard: jobs.map((job) => ({ text: job.name, callback_data: `job_${job.id}` })).map((button) => [button]),
+			},
+		});
+	} catch (error) {
+		console.error(error);
+		await ctx.reply(getText('error', lang));
+	}
+});
