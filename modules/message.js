886b6394dbabf3140b0863fcc112a7281782fd62
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-01-14 18:45:27 +0100

    [feature] Add message module for formatting module and error texts

---
+++ modules/message.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+const languages = JSON.parse(fs.readFileSync('languages.json', 'utf8'));
+
+const getModulesText = (modules) => {
+	console.log(modules);
+	const text = modules
+		.map((module) => {
+			return `*${module.name}*`;
+		})
+		.join('\n');
+	return text;
+};
+
+const getErrorText = (lang) => {
+	return languages[lang].error;
+};
+
+module.exports = {
+	getModulesText,
+	getErrorText,
+};
