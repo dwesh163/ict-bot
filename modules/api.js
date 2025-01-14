47fb7e9812e3d55121261e14b258a1902c00abb4
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-01-14 18:45:14 +0100

    [move] API module

---
+++ modules/api.js
@@ -0,0 +1,40 @@
+const apiURL = 'api-ict.kooked.app';
+
+const getModules = async (lang = 'de') => {
+	console.log(`https://${apiURL}/modules?lang=${lang}`);
+	const response = await fetch(`https://${apiURL}/modules?lang=${lang}`);
+	const data = await response.json();
+	return data;
+};
+
+const getModuleByJob = async (jobId, lang = 'de') => {
+	const response = await fetch(`https://${apiURL}/modules?jobId=${jobId}&lang=${lang}`);
+	const data = await response.json();
+	return data;
+};
+
+const getModuleByYear = async (year, lang = 'de') => {
+	const response = await fetch(`https://${apiURL}/modules?year=${year}&lang=${lang}`);
+	const data = await response.json();
+	return data;
+};
+
+const getModule = async (id, lang = 'de') => {
+	const response = await fetch(`https://${apiURL}/modules/${id}?lang=${lang}`);
+	const data = await response.json();
+	return data;
+};
+
+const getJobs = async (lang = 'de') => {
+	const response = await fetch(`https://${apiURL}/jobs?lang=${lang}`);
+	const data = await response.json();
+	return data;
+};
+
+module.exports = {
+	getModules,
+	getModuleByJob,
+	getModuleByYear,
+	getModule,
+	getJobs,
+};
