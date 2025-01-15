5ccb19f777602375bab630ce2e5f381f82f7153f
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-01-15 15:01:57 +0100

    [feature] Refactor bot commands into separate module for improved organization and maintainability

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const { Telegraf, Markup } = require('telegraf');
 const dotenv = require('dotenv');
-const { getJobs, getModuleByJob, getModule } = require('./modules/api');
-const { getModulesText, getText, getJobsText, getModuleText } = require('./modules/message');
-const { getLang, setLang } = require('./modules/languages');
+const { getText } = require('./modules/message');
+const { getLang } = require('./modules/languages');
+const { modulesCommand, moduleCommand, jobsCommand, languageCommand, callback } = require('./modules/command');
 
 dotenv.config();
 
@@ -10,115 +10,22 @@ console.log('Starting bot');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => ctx.reply(getText('start', getLang(ctx.from.id)), { parse_mode: 'Markdown' }));
+
 bot.help((ctx) => ctx.reply(getText('help', getLang(ctx.from.id)), { parse_mode: 'Markdown' }));
+bot.command('h', (ctx) => ctx.reply(getText('help', getLang(ctx.from.id)), { parse_mode: 'Markdown' }));
 
-bot.command('modules', async (ctx) => {
-	try {
-		const lang = getLang(ctx.from.id);
-		const jobs = await getJobs(lang);
+bot.command('list', modulesCommand);
+bot.command('l', modulesCommand);
+bot.command('all', modulesCommand);
+bot.command('modules', modulesCommand);
 
-		await ctx.reply(getText('jobs_modules', lang), {
-			parse_mode: 'Markdown',
-			reply_markup: {
-				inline_keyboard: jobs
-					.map((job) => ({
-						text: job.name.replace(getText('job_title', lang), '').charAt(0).toUpperCase() + job.name.replace(getText('job_title', lang), '').slice(1),
-						callback_data: `job_${job.id}`,
-					}))
-					.map((button) => [button]),
-			},
-		});
-	} catch (error) {
-		console.error(error);
-		await ctx.reply(getText('error', getLang(ctx.from.id)));
-	}
-});
+bot.command('module', moduleCommand);
 
-bot.command('module', async (ctx) => {
-	try {
-		const lang = getLang(ctx.from.id);
-		const module = await getModule(ctx.message.text.split(' ')[1], lang);
-		if (!module) {
-			await ctx.reply(getText('module_not_found', lang));
-			return;
-		}
+bot.command('jobs', jobsCommand);
 
-		const text = getModuleText(module, lang);
-		await ctx.reply(text, {
-			parse_mode: 'Markdown',
-		});
-	} catch (error) {
-		console.error(error);
-		await ctx.reply(getText('error', getLang(ctx.from.id)));
-	}
-});
+bot.command('language', languageCommand);
 
-bot.command('jobs', async (ctx) => {
-	try {
-		const lang = getLang(ctx.from.id);
-		const jobs = await getJobs(lang);
-		const formatted = getJobsText(jobs, lang);
-
-		await ctx.reply(formatted, {
-			parse_mode: 'Markdown',
-		});
-	} catch (error) {
-		console.error(error);
-		await ctx.reply(getText('error', getLang(ctx.from.id)));
-	}
-});
-
-bot.command('language', async (ctx) => {
-	try {
-		const lang = getLang(ctx.from.id);
-		await ctx.reply(getText('choose_language', lang || 'de'), {
-			reply_markup: {
-				inline_keyboard: [
-					[
-						{ text: '🇩🇪 Deutsch', callback_data: 'lang_de' },
-						{ text: '🇮🇹 Italiano', callback_data: 'lang_it' },
-						{ text: '🇫🇷 Français', callback_data: 'lang_fr' },
-					],
-				],
-			},
-		});
-	} catch (error) {
-		console.error(error);
-		await ctx.reply(getText('error', getLang(ctx.from.id)));
-	}
-});
-
-bot.on('callback_query', async (ctx) => {
-	try {
-		const callbackData = ctx.callbackQuery.data;
-
-		if (callbackData.startsWith('job_')) {
-			const id = callbackData.split('_')[1];
-			const lang = getLang(ctx.from.id);
-			const modules = await getModuleByJob(id, lang);
-			const messages = getModulesText(modules, lang);
-
-			await ctx.editMessageText(messages[0], {
-				parse_mode: 'Markdown',
-			});
-
-			for (let i = 1; i < messages.length; i++) {
-				await ctx.reply(messages[i], {
-					parse_mode: 'Markdown',
-				});
-			}
-		} else if (callbackData.startsWith('lang_')) {
-			const lang = callbackData.split('_')[1];
-			setLang(ctx.from.id, lang);
-			await ctx.editMessageText(getText('language_changed', lang), {
-				parse_mode: 'Markdown',
-			});
-		}
-	} catch (error) {
-		console.error(error);
-		await ctx.answerCbQuery(getText('error', getLang(ctx.from.id)));
-	}
-});
+bot.on('callback_query', callback);
 
 bot.launch();
 
---
+++ modules/command.js
@@ -0,0 +1,119 @@
+const { getJobs, getModuleByJob, getModule } = require('./api');
+const { getModulesText, getText, getJobsText, getModuleText } = require('./message');
+const { getLang, setLang } = require('./languages');
+
+const modulesCommand = async (ctx) => {
+	try {
+		const lang = getLang(ctx.from.id);
+		const jobs = await getJobs(lang);
+
+		await ctx.reply(getText('jobs_modules', lang), {
+			parse_mode: 'Markdown',
+			reply_markup: {
+				inline_keyboard: jobs
+					.map((job) => ({
+						text: job.name.replace(getText('job_title', lang), '').charAt(0).toUpperCase() + job.name.replace(getText('job_title', lang), '').slice(1),
+						callback_data: `job_${job.id}`,
+					}))
+					.map((button) => [button]),
+			},
+		});
+	} catch (error) {
+		console.error(error);
+		await ctx.reply(getText('error', getLang(ctx.from.id)));
+	}
+};
+
+const moduleCommand = async (ctx) => {
+	try {
+		const lang = getLang(ctx.from.id);
+		const module = await getModule(ctx.message.text.split(' ')[1], lang);
+		if (!module) {
+			await ctx.reply(getText('module_not_found', lang));
+			return;
+		}
+
+		const text = getModuleText(module, lang);
+		await ctx.reply(text, {
+			parse_mode: 'Markdown',
+		});
+	} catch (error) {
+		console.error(error);
+		await ctx.reply(getText('error', getLang(ctx.from.id)));
+	}
+};
+
+const jobsCommand = async (ctx) => {
+	try {
+		const lang = getLang(ctx.from.id);
+		const jobs = await getJobs(lang);
+		const formatted = getJobsText(jobs, lang);
+
+		await ctx.reply(formatted, {
+			parse_mode: 'Markdown',
+		});
+	} catch (error) {
+		console.error(error);
+		await ctx.reply(getText('error', getLang(ctx.from.id)));
+	}
+};
+
+const languageCommand = async (ctx) => {
+	try {
+		const lang = getLang(ctx.from.id);
+		await ctx.reply(getText('choose_language', lang || 'de'), {
+			reply_markup: {
+				inline_keyboard: [
+					[
+						{ text: '🇩🇪 Deutsch', callback_data: 'lang_de' },
+						{ text: '🇮🇹 Italiano', callback_data: 'lang_it' },
+						{ text: '🇫🇷 Français', callback_data: 'lang_fr' },
+					],
+				],
+			},
+		});
+	} catch (error) {
+		console.error(error);
+		await ctx.reply(getText('error', getLang(ctx.from.id)));
+	}
+};
+
+const callback = async (ctx) => {
+	try {
+		const callbackData = ctx.callbackQuery.data;
+
+		if (callbackData.startsWith('job_')) {
+			const id = callbackData.split('_')[1];
+			const lang = getLang(ctx.from.id);
+			const modules = await getModuleByJob(id, lang);
+			const messages = getModulesText(modules, lang);
+
+			await ctx.editMessageText(messages[0], {
+				parse_mode: 'Markdown',
+			});
+
+			for (let i = 1; i < messages.length; i++) {
+				await ctx.reply(messages[i], {
+					parse_mode: 'Markdown',
+				});
+			}
+		} else if (callbackData.startsWith('lang_')) {
+			const lang = callbackData.split('_')[1];
+			setLang(ctx.from.id, lang);
+			await ctx.editMessageText(getText('language_changed', lang), {
+				parse_mode: 'Markdown',
+			});
+		}
+	} catch (error) {
+		console.error(error);
+		await ctx.answerCbQuery(getText('error', getLang(ctx.from.id)));
+	}
+};
+
+module.exports = {
+	modulesCommand,
+	moduleCommand,
+	jobsCommand,
+	languageCommand,
+	callback,
+};
